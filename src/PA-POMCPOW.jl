22bd40916ce8356255ad9d8d8ab80831584b2435
jmern3250 <jmern91@stanford.edu>
2021-02-26 11:51:13 -0800

    cleaned development files

--- src/PA-POMCPOW.jl
+++ src/PA-POMCPOW.jl
@@ -7,67 +7,14 @@ using POMCPOW
 using POMDPPolicies
 using POMDPSimulators
 
-using GaussianProcesses
 using Random
 using Distributions
 using StatsBase
 using Parameters
 using CPUTime
-using Plots
 
 import Base: abs
 
-include("./utils/misc.jl")
-export
-        average,
-        savefig_recursive,
-        cartind_to_array,
-        reshape_gp_samples,
-        euclidean_dist
-
-include("./utils/linexp_kernel.jl")
-export
-        LEIso
-
-include("beliefstates.jl")
-export
-        GaussianProcessBelief,
-        GaussianProcessUpdater,
-        GaussianProcessSolve,
-        GaussianProcessSample,
-        GroundTruthUpdater,
-        GaussianProcessState,
-        RolloutPolicy,
-        RolloutUpdater,
-        WildfireBelief,
-        WildfireBeliefUpdater
-
-include("./pomdps/gaussianpomdp.jl")
-export
-        GaussianPOMDP,
-        action_values,
-        obs_weight
-
-include("wildfirepomdp.jl")
-export
-        WildfirePOMDP,
-        action_values,
-        obs_weight,
-        plot_state,
-        action_values
-
-include("./baselines/greedysolvers.jl")
-export
-        GreedyPolicy,
-        action,
-        GreedyUpdater
-
-include("./baselines/pa_pomcp.jl")
-export
-        DAEPOMCPSolver,
-        DAEPolicy,
-        AEPolicy,
-        DAEPOMCPPlanner
 
 include("pa_pomcpow.jl")
 export
@@ -78,12 +25,7 @@ export
 include("action_select.jl")
 export
         ActionSelector,
-        average,
-        sigmoid,
-        expo_activator,
-        normalize!,
-        stretch!,
-        Idx_to_CartInd,
-        ScenarioSimulator,
-        savefig_recursive
+        embed_actions, 
+        max_action,
+        sample_actions
 end # module
--- test/runtests.jl
+++
@@ -1,6 +0,0 @@
-using ResourceAllocation
-using Test
-
-@testset "ResourceAllocation.jl" begin
-    # Write your own tests here.
-end
